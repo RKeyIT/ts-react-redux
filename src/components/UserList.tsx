91ecd77500c10f60780fe3afdaf768911a81bdca
Raman Krytski <rkeydevelop@gmail.com>
2023-01-12 06:17:23 +0100

    feat: created custom hook useTypedSelecter form useSelector | typed reducer | added state to app

---
+++ src/components/UserList.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {useTypedSelector} from "../hooks/useTypeSelector";
+
+const UserList: React.FC = () => {
+    const {users, loading, error} = useTypedSelector(state => state.user)
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default UserList;
