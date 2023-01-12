5450c1c17ca14c40b3e87d4a346ab926dd8db23b
Raman Krytski <rkeydevelop@gmail.com>
2023-01-12 06:55:02 +0100

    feat: created users request and defien bug with dispatch function into useEffect

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useTypedSelector} from "../hooks/useTypeSelector";
+import {useDispatch} from "react-redux";
+import {fetchUsers} from "../action-creators/user";
 
 const UserList: React.FC = () => {
     const {users, loading, error} = useTypedSelector(state => state.user)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(fetchUsers())
+    }, [])
+
+    if(loading) {
+        return <h1>Loading...</h1>
+    }
+    if (error) {
+        return <h2>Error { error }</h2>
+    }
+
     return (
         <div>
-
+            {users.map( user => <div key={user.id}>{user.name}</div> )}
         </div>
     );
 };
